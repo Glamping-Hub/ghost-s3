500842f42ed58b6d4538c1839e3c60854ee4ee08
Alexandru Rosianu <rosian_alex@yahoo.com>
2014-12-17 17:02:07 +0200

    Updates the code to support ghost 0.5.7; also, this is not an npm package anymore

--- README.md
+++ README.md
@@ -1,41 +1,26 @@
-# Ghost S3
+# Ghost: S3 File Store module
 
-This is an S3 file module for Ghost blogs.
-
-
+AWS S3 file storage module for Ghost blogs.
 
 ## Install
 
-	npm install --save ghost-s3
-
+1. Copy the `s3.js` file to `core/server/storage/`
+2. Change `storageChoice` in `core/server/storage/index.js:7` to `s3`
+3. Add `aws-sdk` to dependencies in your `package.json`
 
 ## Configure
 
-In your ghost config.js file under "development" and "production" add
+In your Ghost config.js file, under "development" and/or "production", add:
 
 	aws: {
-	    accessKeyId: 'your aws access key id>',
-	    secretAccessKey: 'your AWS secret access key>',
-	    bucket: 'your-bucket-name',
-	    region: 'the AWS region your bucket is in'
-	},
+	    accessKeyId: 'your AWS access key id',
+	    secretAccessKey: 'your AWS secret access key',
+	    bucket: 'your bucket name',
+	    region: 'the AWS region your bucket is in; e.g.: eu-west-1'
+	}
 
+Note: for enhanced security, it is recommended that you use environment variables instead of hard-coding the values in your config.js file.
 
-## Plug In
+## Updates
 
-Until Ghost has a file module system, you will have to change the file ```storage/index```
-
-```javascript
-	storage = require('./' + storageChoice);
-```
-
-becomes
-
-```javascript
-	storage = require('ghost-s3')({
-	    errors: errors,
-	    config: require('../config')().aws
-	});
-```
-
-
+Until Ghost has a plugin system in place, this will be the only way to use S3 storage for your Ghost blog. I will try to update this repository to support future versions of Ghost.
---
+++ s3.js
@@ -0,0 +1,64 @@
+// # S3 Image Storage module
+// Module for storing images using an AWS S3 bucket
+
+var fs        = require('fs'),
+    path      = require('path'),
+    util      = require('util'),
+    Promise   = require('bluebird'),
+    AWS       = require('aws-sdk'),
+    errors    = require('../errors'),
+    config    = require('../config'),
+    baseStore = require('./base'),
+    s3        = new AWS.S3(config.aws);
+
+function S3FileStore() {}
+util.inherits(S3FileStore, baseStore);
+
+// Saves the image into the bucket
+// - image is the express image object
+// - returns a promise which ultimately returns the full url to the uploaded image
+S3FileStore.prototype.save = function (image) {
+    var filename = this.getUniqueFileKey(image);
+    var readFile = Promise.promisify(fs.readFile);
+    var putObject = Promise.promisify(s3.putObject.bind(s3));
+
+    return readFile(image.path).then(function (buffer) {
+        return putObject({
+            Bucket: config.aws.bucket,
+            Key: filename,
+            Body: buffer,
+            ContentType: image.type,
+            CacheControl: 'max-age=' + (30 * 24 * 60 * 60) // 30 days
+        });
+    }).then(function (result) {
+        return 'https://' + config.aws.bucket + '.s3.amazonaws.com/' + filename;
+    }).catch(function (e) {
+        errors.logError(e);
+        return Promise.reject(e);
+    });
+};
+
+// Generate a unique Key used to store the S3 object
+S3FileStore.prototype.getUniqueFileKey = function (image) {
+    var ext = path.extname(image.name),
+    name = path.basename(image.name, ext).replace(/[\W]/gi, '-')
+    timestamp = Date.now();
+
+    return path.join(this.getTargetDir(), name + '-' + timestamp + ext);
+};
+
+// Always return false since filenames are already unique
+S3FileStore.prototype.exists = function (url) {
+    return new Promise(function (resolve) {
+        resolve(false);
+    });
+};
+
+// No serving needed, so just do next()
+S3FileStore.prototype.serve = function () {
+    return function (req, res, next) {
+        next();
+    };
+};
+
+module.exports = S3FileStore;
